6782789435a53279633d5805d1ef59a97e2f285b
Absolute Zero <supayu99@gmail.com>
2026-01-05 12:41:52 +0700

    section Title scene

---
+++ nourishment.py
@@ -0,0 +1,47 @@
+from manim import *
+import numpy as np
+
+class sectionTitle(Scene):
+    def construct(self):
+
+      title = Tex("Lesson Overview", font_size=36*1.5, color=WHITE)
+      title.to_corner(UL, buff=0.5)
+      section_title = Tex(r"$\S$2. Soft engineering", font_size = 60).move_to(ORIGIN)
+   
+      items = [
+          Tex("1. Preliminary discussion on importance", font_size=54, color=WHITE),
+          Tex("2. Soft engineering", font_size=54, color=YELLOW),
+          Tex("3. Look into the future", font_size=54, color=WHITE)
+      ]
+      agenda = VGroup(*items).arrange(DOWN, aligned_edge=LEFT, buff=0.4).to_edge(LEFT).shift(UP * 1)
+
+      self.wait(1)
+      self.play(FadeIn(agenda, title))
+      self.wait(2)
+      self.play(ReplacementTransform(VGroup(agenda, title), section_title))
+      self.wait(2)
+      self.play(FadeOut(section_title))
+      self.wait(2)
+
+ 
+
+class seperation(Scene):
+    def construct(self):
+        return
+    
+class beach(Scene):
+    def construct(self):
+        return
+
+
+class dune(Scene):
+    def construct(self):
+        return
+
+class example(Scene):
+    def construct(self):
+        return
+
+class conclusion(Scene):
+    def construct(self):
+        return
