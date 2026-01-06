d55ab19c9405890b49be347946baadcef3ab8630
Absolute Zero <supayu99@gmail.com>
2026-01-06 12:12:36 +0700

    initial commit

---
+++ vetiver.py
@@ -0,0 +1,13 @@
+from manim import *
+
+class introduction(Scene):
+    def construct(self):
+        return
+
+class advantages(Scene):
+    def construct(self):
+        return
+    
+class applications(Scene):
+    def construct(self):
+        return
